d5c1332a4a9a5ab7d22f59e0547881546e9de0cb
conconrob <connortprobertson@gmail.com>
2019-01-23 12:04:27 +0000

    init commit

--- assignments/this.js
+++ assignments/this.js
@@ -6,21 +6,32 @@
 * 3. 
 * 4. 
 *
-* write out a code example of each explanation above
+*write out a code example of each explanation above
 */
 
 // Principle 1
 
 // code example for Window Binding
-
+//this.alert("I called this using this") //calls the alert function from the window object 
 // Principle 2
 
 // code example for Implicit Binding
 
+const dog = {
+    name: "Spook",
+    speak: function(){
+        console.log(`My name is ${this.name}. This was said using implicit binding`);
+    }
+};
+dog.speak();
 // Principle 3
 
 // code example for New Binding
-
+const Man = function(obj){
+    this.name = obj.name;
+}
+const connor = new Man({name:"connor"});
+console.log(connor, "was created with new bindings");
 // Principle 4
 
 // code example for Explicit Binding
